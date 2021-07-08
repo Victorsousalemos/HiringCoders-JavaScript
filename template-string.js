2d5481d0e4c03758ebc67b8c1b3cf3a00d85f7f8
Victor de Sousa Lemos <victorsousalemos@hotmail.com>
2021-07-08 15:52:32 -0300

    Template String
    Aprendendo a usar o Template String

---
+++ template-string.js
@@ -0,0 +1,10 @@
+// Template String só usa as crases e o sinal de dolar e chave
+
+'Victor é'
+let role = 'Inteligente'
+
+console.log(`Victor é ${role}`)
+
+// Antes tinha que concatena
+
+console.log('Victor é' + ' ' + role)
